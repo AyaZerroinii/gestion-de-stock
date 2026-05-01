23d8fff2c40e22bc47413ba26923de82113d9c10
Nour <norhane.ramdane@etu.univ-batna2.dz>
2026-05-01 10:08:27 +0100

    read ALLOWED_HOSTS from environment

--- gestion_stock/settings.py
+++ gestion_stock/settings.py
@@ -132,7 +132,7 @@ CACHES = {
 # ========== EMAIL CONFIGURATION ==========
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER='ayazrrou@gmail.com'
 EMAIL_HOST_PASSWORD='gyry grmd oecq edqc'
